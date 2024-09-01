dd760ba404d7b679b0fb46aa0abaaa05f40b329c
bye <bye@byecorps.com>
2024-09-01 12:01:44 +0100

    Betetr camera!!

--- src/camera.js
+++ src/camera.js
@@ -4,22 +4,46 @@ export default class Camera {
 
         this.x = 0;
         this.y = 0;
+
+        this.goingToX = 0;
+        this.goingToY = 0;
+        this.startX = 0;
+        this.startY = 0;
+        this.duration = 0; // ms
+        this.moveStart =0; // Start of move in ms
+        this.moveEnd = 0;
     }
 
     get height() {
         return this.c.height - this.y;
     }
 
-    gotoY(y) {
-        this.y = Math.round(y);
+    goTo(x, y, t=0) {
+        this.goingToX = x;
+        this.goingToY = y;
+        this.startX = this.x;
+        this.startY = this.y;
+        this.moveStart = performance.now();
+        this.moveEnd = performance.now() + t;
     }
 
-    gotoX(x) {
-        this.x = Math.round(x);
-    }
+    step() {
+        const elapsed = Math.min(performance.now() - this.moveStart, this.moveEnd);
+        const end = this.moveEnd - this.moveStart;
+        const pos = (elapsed / end) || 1;
 
-    goTo(x, y) {
-        this.x = x;
-        this.y = y;
+        const distX = this.goingToX - this.startX;
+        const distY = this.goingToY - this.startY;
+
+        this.x = Math.min(
+            this.goingToX,
+            this.startX + (distX * pos)
+            );
+        this.y = Math.min(
+            this.goingToY,
+            this.startY + (distY * pos)
+        );
+
+        console.log(elapsed, end, pos, distX, distY)
     }
 }
